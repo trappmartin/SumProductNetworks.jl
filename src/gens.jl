7e74895f37bc5fdd4aec36e01259188544cab932
Martin Trapp <trapp.martin@gmail.com>
2016-01-28 15:57:40 +0100

    finished learnSPN implementation by Gens. This version is slightly more Bayesian than the version by Gens and is meant for continuous data.

--- src/gens.jl
+++ src/gens.jl
@@ -26,6 +26,8 @@ function learnSumNode{T <: Real}(X::Array{T}, G0::ConjugatePostDistribution; ite
 
 	# NOTE: This seems to break sometimes.. not sure why and when
 	psm = compute_psm(Z)
+
+	# NOTE: The average method of hclust seems to be buggy, but the complete works fine.
 	(idx, value) = point_estimate(psm, method = :comp)
 
 	# number of child nodes
@@ -78,5 +80,88 @@ function learnProductNode{T <: Real}(X::Array{T}; method = :HSCI, pvalue = 0.1)
 	end
 
 	return indset
+
+end
+
+function learnSPN{T <: Real}(X::Array{T, 2}, mapping::Dict{Int, Int}; parent = Nullable{ProductNode}(), minSamples = 10)
+
+	# learn SPN using Gens learnSPN
+	(D, N) = size(X)
+	dimMapping = [convert(Int, d) => mapping[d] for d in 1:D]
+
+	# define G0
+	μ0 = vec( mean(X, 2) )
+	κ0 = 1.0
+	ν0 = 4.0
+	Ψ = eye(D) * 10
+	G0 = GaussianWishart(μ0, κ0, ν0, Ψ)
+
+	# learn sum nodes
+	(w, ids) = learnSumNode(X, G0, minN = minSamples)
+
+	# create sum node
+	snode = SumNode(0)
+
+	# check if this is supposed to be the root
+	if !isnull(parent)
+		p = get(parent)
+		add!(p, snode)
+	end
+
+	uidset = unique(ids)
+
+	for uid in uidset
+
+		# add product node
+		node = ProductNode(uid)
+		add!(snode, node, w[uid])
+
+		Xhat = X[:,ids .== uid]
+
+		# compute product nodes
+		Dhat = Set(learnProductNode(Xhat))
+		Dset = Set(1:D)
+
+		Ddiff = setdiff(Dset, Dhat)
+
+		# get list of children
+		if length(Ddiff) > 0
+			# split has been found
+
+			# don't recurse if only one dimension is inside the bucket
+			if length(Ddiff) == 1
+				d = pop!(Ddiff)
+				leaf = UnivariateNode{ConjugatePostDistribution}(BNP.add_data(NormalGamma(μ = mean(Xhat[d,:])), Xhat[d,:]), scope = dimMapping[d])
+				add!(node, leaf)
+			else
+				# recurse
+				learnSPN(Xhat[collect(Ddiff),:], dimMapping, parent = Nullable(node))
+			end
+
+			# don't recurse if only one dimension is inside the bucket
+			if length(Dhat) == 1
+				d = pop!(Dhat)
+				leaf = UnivariateNode{ConjugatePostDistribution}(BNP.add_data(NormalGamma(μ = mean(Xhat[d,:])), Xhat[d,:]), scope = dimMapping[d])
+				add!(node, leaf)
+			else
+				# recurse
+				learnSPN(Xhat[collect(Dhat),:], dimMapping, parent = Nullable(node))
+			end
+
+		else
+
+			# construct leaf nodes
+			for d in Dhat
+				leaf = UnivariateNode{ConjugatePostDistribution}(BNP.add_data(NormalGamma(μ = mean(Xhat[d,:])), Xhat[d,:]), scope = dimMapping[d])
+				add!(node, leaf)
+			end
+
+		end
+
+	end
+
+	if isnull(parent)
+		return snode
+	end
 
 end
--- test/genstests.jl
+++ test/genstests.jl
@@ -34,55 +34,12 @@ Dhat = SPN.learnProductNode(X)
 
 println(" * learn SPN using learnSPN..")
 
-# learn SPN using Gens learnSPN
 (D, N) = size(X)
-dimMapping = [d => d for d in 1:D]
 
-# define G0
-μ0 = vec( mean(X, 2) )
-κ0 = 1.0
-ν0 = 4.0
-Ψ = eye(D) * 10
-G0 = GaussianWishart(μ0, κ0, ν0, Ψ)
+mapping = Dict{Int, Int}([convert(Int, d) => convert(Int, d) for d in 1:D])
 
-# learn sum nodes
-(w, ids) = SPN.learnSumNode(X, G0)
+root = SPN.learnSPN(X, mapping)
 
-# create sum node
-root = SumNode(0)
-
-uidset = unique(ids)
-
-for uid in uidset
-
-	# add product node
-	node = ProductNode(uid)
-	add!(root, node, w[uid])
-
-	Xhat = X[:,ids .== uid]
-
-	# compute product nodes
-	Dhat = Set(SPN.learnProductNode(Xhat))
-	Dset = Set(1:D)
-
-	Ddiff = setdiff(Dset, Dhat)
-
-	# get list of children
-	if length(Ddiff) > 0
-		# split has been found
-
-		# recurse
-
-	else
-
-		# construct leaf nodes
-		for d in Dhat
-			leaf = UnivariateNode{ConjugatePostDistribution}(BNP.add_data(NormalGamma(μ = mean(Xhat[d,:])), Xhat[d,:]), scope = dimMapping[d])
-			add!(node, leaf)
-		end
-
-	end
-
-end
+println(" * draw SPN")
 
 draw(root, file = "learnSPN.svg")
