bfca5149fc7685995ecb43fd630a56a38123f353
Martin Trapp <trapp.martin@gmail.com>
2017-02-15 16:31:21 +0100

    reversed to Gibbs, SliceSampler might be brocken

--- src/gens.jl
+++ src/gens.jl
@@ -13,7 +13,7 @@ function learnSumNode{T <: AbstractFloat}(X::AbstractArray{T}; iterations = 1000
 	ν0 = 9.0
 	Σ0 = cov(X) + (10 * eye(D))
 
-	models = train(init(X, DPM(GaussianDiagonal{NormalNormal}(NormalNormal[NormalNormal(μ0 = mean(X[:,d])) for d in 1:D])), KMeansInitialisation(k = round(Int, log(N)))), DPMHyperparam(), SliceSampler(maxiter = iterations))
+	models = train(init(X, DPM(GaussianDiagonal{NormalNormal}(NormalNormal[NormalNormal(μ0 = mean(X[:,d])) for d in 1:D])), KMeansInitialisation(k = round(Int, log(N)))), DPMHyperparam(), Gibbs(maxiter = iterations))
 
 	ass = models[end].assignments
 
