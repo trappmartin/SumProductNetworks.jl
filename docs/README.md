c58d24ca5c485c6bf28ddea27cf259a1b3ddc6e0
Martin Trapp <trapp.martin@gmail.com>
2017-07-12 13:52:01 +0200

    added internal bmi code

---
+++ docs/README.md
@@ -0,0 +1,2 @@
+# Sum-Product Networks in Julia
+This is the home of the software package SumProductNetworks.jl implementing Sum-Product Networks in julia.
---
+++ src/bmiTest.jl
@@ -0,0 +1,50 @@
+"""
+	Test if discrete sets are independent.
+
+	test(X, Y) -> p, log(P)
+
+	Computes the Independence Test by Margaritis and Thrun for X and Y.
+	The result p gives the probability of X and Y being independent. And
+	log(p) is the logarithm of p.
+
+"""
+function test(X::Vector{Int}, Y::Vector{Int}; α = 1.0)
+
+	uniqueX = unique(X)
+	uniqueY = unique(Y)
+
+	if any(uniqueX .≠ 1:length(uniqueX))
+	    t = zeros(size(X));
+	    for k = 1:length(uniqueX)
+	        t[X .== uniqueX[k]] = k
+	    end
+	    X = copy(t)
+	end
+
+	if any(uniqueY .≠ 1:length(uniqueY))
+	    t = zeros(size(Y));
+	    for k = 1:length(uniqueY)
+	        t[Y .== uniqueY[k]] = k
+	    end
+	    Y = copy(t)
+	end
+
+	L  = length(X)
+	NX = length(uniqueX)
+	NY = length(uniqueY)
+
+	(eX, hX) = hist(X, 0:NX)
+	(eY, hY) = hist(Y, 0:NY)
+	(e, h)  = hist((X-1)*NY + Y, 0:(NX*NY))
+
+	PRD = lgamma(α*NX*NY) - lgamma(α*NX*NY + L) + sum( lgamma(h  + α) ) - NX*NY*lgamma(α)
+	PRI = lgamma(α*NX)    - lgamma(α*NX    + L) + sum( lgamma(hX + α) ) - NX * lgamma(α) +
+	      lgamma(α*NY)    - lgamma(α*NY    + L) + sum( lgamma(hY + α) ) - NY * lgamma(α)
+
+	m = maximum([PRD, PRI])
+
+	logP = PRI-m - log(exp(PRD-m) + exp(PRI-m))
+	p = exp(logP)
+
+	return (p, logP)
+end
