834aee6feffa41aa0dd34a9b629ad0ccc9efb562
Martin Trapp <trapp.martin@gmail.com>
2018-08-31 19:05:16 +0100

    minor clean-up

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,5 @@
 using SumProductNetworks
 
-@info("[runtests.jl] runtests.jl loaded")
-
 testcases = Dict(
         "utilities"  => ["utilityFunctionTest"],
         "layers"     => ["layersTests", "bayesianLayerTests", "layerFunctionsTests"]
@@ -12,15 +10,9 @@ testcases = Dict(
 path = dirname(@__FILE__)
 cd(path)
 
-@info("[runtests.jl] CDed test path")
-@info("[runtests.jl] testing starts")
-
 for (target, list) in testcases
   for t in list
     filename = string(t, ".jl")
-    @info("[runtests.jl] \"$target/$t.jl\" starts")
     include( joinpath(target, filename) );
-    @info("[runtests.jl] \"$target/$t.jl\" finished")
   end
 end
-@info("[runtests.jl] all tests processed")
