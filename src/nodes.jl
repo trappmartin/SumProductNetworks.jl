5134662a4a7043179e830ff368224a8a9bbf2a87
Martin Trapp <trapp.martin@gmail.com>
2019-02-05 17:43:35 +0100

    Further clean up and added some docstrings.

--- src/nodes.jl
+++ src/nodes.jl
@@ -7,9 +7,11 @@ export MultivariateNode
 # Abstract definition of an SumProductNetwork node.
 abstract type SPNNode end
 abstract type Node <: SPNNode end
-abstract type SumNode{T} <: Node end
+abstract type SumNode <: Node end
 abstract type ProductNode <: Node end
 abstract type Leaf <: SPNNode end
+
+header(node::SPNNode) = "$(summary(node))($(node.id))"
 
 function Base.show(io::IO, node::SPNNode)
     println(io, header(node))
@@ -27,8 +29,14 @@ function Base.show(io::IO, node::SPNNode)
     end
 end
 
+function Base.show(io::IO, node::Leaf)
+    println(io, header(node))
+    println(io, "\tscope = $(scope(node))")
+    println(io, "\tparameters = $(params(node))")
+end
+
 struct SumProductNetwork
-    root::SumNode
+    root::Node
     nodes::Vector{SPNNode}
     leaves::Vector{Leaf}
     idx::Dict{Symbol,Int}
@@ -65,29 +73,55 @@ function Base.show(io::IO, spn::SumProductNetwork)
     println(io, "\tdepth = $(length(spn.layers))")
 end
 
-# A finite sum node.
-mutable struct FiniteSumNode{T <: Real} <: SumNode{T}
+"""
+   FiniteSumNode <: SumNode
+   
+A sum node computes a convex combination of its weight and the pdf's of its children.
+
+## Usage:
+
+```julia
+node = FiniteSumNode{Float64}(;D=4, N=1)
+add!(node, ..., log(0.5))
+add!(node, ..., log(0.5))
+logpdf(node, rand(4))
+```
+
+"""
+mutable struct FiniteSumNode{T<:Real} <: SumNode
     id::Symbol
     parents::Vector{SPNNode}
     children::Vector{SPNNode}
     logweights::Vector{T}
-    α::Union{Float64, Vector{Float64}}
     scopeVec::BitArray{1}
     obsVec::BitArray{1}
 end
 
-function FiniteSumNode{T}(;D=1, N=1, parents = SPNNode[], α = 1.) where T <: Real
-    return FiniteSumNode{T}(gensym(:sum), parents, SPNNode[], T[], α, falses(D), falses(N))
+function FiniteSumNode{T}(;D=1, N=1, parents=SPNNode[]) where {T<:Real}
+    return FiniteSumNode{T}(gensym(:sum), parents, SPNNode[], T[], falses(D), falses(N))
 end
-function FiniteSumNode(;D=1, N=1, parents=SPNNode[], α=1.)
-    return FiniteSumNode{Float64}(;D=D, N=N, parents=parents, α=α)
+function FiniteSumNode(;D=1, N=1, parents=SPNNode[])
+    return FiniteSumNode{Float64}(;D=D, N=N, parents=parents)
 end
 
-header(node::SPNNode) = "$(summary(node))($(node.id))"
-eltype(::Type{FiniteSumNode{T}}) where T<:Real = T
+eltype(::Type{FiniteSumNode{T}}) where {T<:Real} = T
 eltype(n::SPNNode) = eltype(typeof(n))
 
-# A finite product node.
+"""
+   FiniteProductNode <: ProductNode
+   
+A product node computes a product of the pdf's of its children.
+
+## Usage:
+
+```julia
+node = FiniteProductNode(;D=4, N=1)
+add!(node, ...)
+add!(node, ...)
+logpdf(node, rand(4))
+```
+
+"""
 struct FiniteProductNode <: ProductNode
     id::Symbol
     parents::Vector{SPNNode}
@@ -100,7 +134,22 @@ function FiniteProductNode(;D=1, N=1, parents = SPNNode[])
     return FiniteProductNode(gensym(:prod), parents, SPNNode[], falses(D), falses(N))
 end
 
-# Definition of an indicater Node.
+"""
+   IndicatorNode <: Leaf
+   
+An indicator node evaluates an indicator function.
+
+## Usage:
+
+```julia
+value = 1
+dimension = 1
+node = IndicatorNode(value, dimension)
+logpdf(node, [1, 2, 1]) # == 0.0
+logpdf(node, [2, 2, 1]) # == -Inf
+```
+
+"""
 mutable struct IndicatorNode <: Leaf
     id::Symbol
     value::Int
@@ -117,7 +166,21 @@ function Base.isequal(n1::IndicatorNode, n2::IndicatorNode)
 end
 params(n::IndicatorNode) = (n.value)
 
-# A univariate node computes the likelihood of x under a univariate distribution.
+"""
+   UnivariateNode <: Leaf
+   
+A univariate node evaluates the logpdf of its univariate distribution function.
+
+## Usage:
+
+```julia
+distribution = Normal()
+dimension = 1
+node = UnivariateNode(distribution, dimension)
+logpdf(node, rand(2)) # == logpdf(Normal(), rand(2))
+```
+
+"""
 mutable struct UnivariateNode <: Leaf
     id::Symbol
     parents::Vector{SPNNode}
@@ -128,16 +191,23 @@ end
 function UnivariateNode(distribution::T, dim::Int; parents = SPNNode[]) where {T<:UnivariateDistribution}
     return UnivariateNode(gensym(:univ), parents, distribution, dim)
 end
-
 params(n::UnivariateNode) = Distributions.params(n.dist)
 
-function Base.show(io::IO, node::Leaf)
-    println(io, header(node))
-    println(io, "\tscope = $(scope(node))")
-    println(io, "\tparameters = $(params(node))")
-end
+"""
+   MultivariateNode <: Leaf
+   
+A multivariate node evaluates the logpdf of its multivariate distribution function.
 
-# A multivariate node computes the likelihood of x under a multivariate distribution.
+## Usage:
+
+```julia
+distribution = MvNormal()
+dimensions = [1, 2]
+node = UnivariateNode(distribution, dimensions)
+logpdf(node, rand(2)) # == logpdf(MvNormal(), rand(2))
+```
+
+"""
 mutable struct MultivariateNode <: Leaf
     id::Symbol
     parents::Vector{SPNNode}
@@ -148,5 +218,4 @@ end
 function MultivariateNode(distribution::T, dims::Vector{Int}; parents = SPNNode[]) where {T<:MultivariateDistribution}
     return MultivariateNode(gensym(:multiv), parents, distribution, dims)
 end
-
 params(n::MultivariateNode) = Distributions.params(n.dist)
